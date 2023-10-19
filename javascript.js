52bf1a1b02a046a8f249464a56166cb092944fde
Rishan-Jadva <rishanjadva@gmail.com>
2023-10-19 12:33:05 +1100

    Outcome Logic Written

--- javascript.js
+++ javascript.js
@@ -9,5 +9,42 @@ function getComputerChoice(){
             return "Rock"
     }
 }
+function round(playerSelection,computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    switch (playerSelection){
+        case "rock":
+            if(computerSelection==="Paper"){
+                return "You Lose! Paper beats Rock";
+            }
+            else if(computerSelection==="Rock"){
+                return "You Tied!";
+            }
+            else{
+                return "You Won! Rock beat Scissors";
+            }
+        case "scissors":
+            if(computerSelection==="Paper"){
+                return "You Win! Scissors beats Paper";
+            }
+            else if(computerSelection==="Rock"){
+                return "You Lose! Rock beats Scissors";
+            }
+            else{
+                return "You Tied!";
+            }
+        case "paper":
+            if(computerSelection==="Paper"){
+                return "You Tied";
+            }
+            else if(computerSelection==="Rock"){
+                return "You Won! Paper beats Rock";
+            }
+            else{
+                return "You Lose! Scissors beats Paper";
+            }
+        default:
+            return "Error!"
+    }
+}
 let choice = getComputerChoice();
 console.log(choice);
