d05e112bfcbc4dae215588fa41b9a5f26eb18cbd
Rishan-Jadva <rishanjadva@gmail.com>
2023-10-19 14:38:06 +1100

    Completed Game

--- javascript.js
+++ javascript.js
@@ -9,42 +9,73 @@ function getComputerChoice(){
             return "Rock"
     }
 }
-function round(playerSelection,computerSelection){
+function playRound(playerSelection,computerSelection){
     playerSelection = playerSelection.toLowerCase();
     switch (playerSelection){
         case "rock":
             if(computerSelection==="Paper"){
-                return "You Lose! Paper beats Rock";
+                return "You Lose!";
             }
             else if(computerSelection==="Rock"){
                 return "You Tied!";
             }
             else{
-                return "You Won! Rock beat Scissors";
+                return "You Won!";
             }
         case "scissors":
             if(computerSelection==="Paper"){
-                return "You Win! Scissors beats Paper";
+                return "You Win!";
             }
             else if(computerSelection==="Rock"){
-                return "You Lose! Rock beats Scissors";
+                return "You Lose!";
             }
             else{
                 return "You Tied!";
             }
         case "paper":
             if(computerSelection==="Paper"){
-                return "You Tied";
+                return "You Tied!";
             }
             else if(computerSelection==="Rock"){
-                return "You Won! Paper beats Rock";
+                return "You Won!";
             }
             else{
-                return "You Lose! Scissors beats Paper";
+                return "You Lose!";
             }
         default:
             return "Error!"
     }
 }
-let choice = getComputerChoice();
-console.log(choice);
+function getPlayerChoice(){
+    return prompt("Rock, Paper or Scissors?");
+}
+function game(){
+    let count = 0;
+    let playerScore=0;
+    let computerScore=0;
+    while (count<5){
+        computerSelection = getComputerChoice();
+        playerSelection = getPlayerChoice();
+        outcome = playRound(playerSelection, computerSelection);
+        if(outcome==="You Won!"){
+            playerScore++;
+        }
+        else if(outcome==="You Lose!"){
+            computerScore++;
+        }
+        count++
+        console.log(outcome);
+        console.log("Your Score: "+playerScore);
+        console.log("Computer Score: "+computerScore);
+    }
+    if(playerScore>computerScore){
+        console.log("Player Wins")
+    }
+    else if(playerScore==computerScore){
+        console.log("You Tied the Game")
+    }
+    else{
+        console.log("Computer Wins")
+    }
+}
+game();
